62f68d6c8ac6777b8f721ff1d81ea7c8830c0a1d
peytontw <wilkiejr29@gmail.com>
2017-11-17 00:41:33 +0000

    third commit

--- comments/public/javascripts/comments.js
+++ comments/public/javascripts/comments.js
@@ -1,47 +1,169 @@
-$(document).ready(function(){
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl);
+//  .directive('expense', expenseDirective);
 
-  $("#postComment").click(function(){
-    var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
-    jobj = JSON.stringify(myobj);
-    $("#json").text(jobj);
-    var url = "comment";
-    $.ajax({
-      url:url,
-      type: "POST",
-      data: jobj,
-      contentType: "application/json; charset=utf-8",
-      success: function(data,textStatus) {
-        $("#done").html(textStatus);
+
+  function mainCtrl ($scope) {
+
+      $scope.rows=[];
+
+      $scope.monthlyTithing = 0;
+      $scope.monthlySavings = 0;
+      $scope.yearlyTithing = 0;
+      $scope.yearlySavings = 0;
+      $scope.savingsPercent = 0;
+      $scope.newMonthly = 0;
+      $scope.newYearly = 0;
+
+      $scope.addIncome = function () {
+
+        $scope.monthlyTithing = $scope.incomeInput.monthlyIncome*.1;
+        $scope.yearlyTithing = ($scope.monthlyTithing*12);
+
+        var totalMonthlyExpenses = 0;
+
+        for(var i=0; i<$scope.rows.length; i++){
+            totalMonthlyExpenses += $scope.rows[i].monthlyExpense;
+        }
+
+        var savingsTotal = ($scope.incomeInput.monthlyIncome - $scope.monthlyTithing);
+        savingsTotal -= totalMonthlyExpenses;
+
+        $scope.monthlySavings = savingsTotal;
+        $scope.yearlySavings = (savingsTotal*12);
+        $scope.savingsPercent = ($scope.monthlySavings / $scope.incomeInput.monthlyIncome * 100);
+    };
+
+    $scope.updateYearlyIncome = function(row) {
+        row.yearlyIncome = row.monthlyIncome * 12;
+    };
+
+    $scope.updateMonthlyIncome = function(row) {
+        row.monthlyIncome = row.yearlyIncome / 12;
+    };
+
+
+    $scope.updateYearlyInput = function(row) {
+        row.yearlyExpense = row.monthlyExpense * 12;
+        row.percent = row.monthlyExpense / $scope.incomeInput.monthlyIncome * 100;
+
+        var totalMonthlyExpenses = 0;
+
+        for (var i=0; i<$scope.rows.length; i++){
+            totalMonthlyExpenses += $scope.rows[i].monthlyExpense;
+        }
+
+
+        var savingsTotal = ($scope.incomeInput.monthlyIncome - $scope.monthlyTithing);
+        savingsTotal -= totalMonthlyExpenses;
+
+        $scope.monthlySavings = savingsTotal;
+        $scope.yearlySavings = (savingsTotal*12);
+        $scope.savingsPercent = (savingsTotal/$scope.incomeInput.monthlyIncome * 100);
+    };
+
+    $scope.updateMonthlyInput = function(row) {
+        row.monthlyExpense = row.yearlyExpense / 12;
+        row.percent = row.monthlyExpense / $scope.incomeInput.monthlyIncome * 100;
+
+        var totalMonthlyExpenses = 0;
+
+        for(var i=0; i<$scope.rows.length; i++){
+            totalMonthlyExpenses += $scope.rows[i].monthlyExpense;
+
+        }
+
+
+        var savingsTotal = ($scope.incomeInput.monthlyIncome - $scope.monthlyTithing);
+        savingsTotal -= totalMonthlyExpenses;
+
+        $scope.monthlySavings = savingsTotal;
+        $scope.yearlySavings = (savingsTotal*12);
+        $scope.savingsPercent = (savingsTotal/$scope.incomeInput.monthlyIncome * 100);
+    };
+
+    $scope.addNew = function (row) {
+
+      if($scope.monthlyTithing == 0){
+          alert("You can't buy things without money, fool ;) Add income first. Thanks.");
       }
-    })
-  });
+      else {
+          $scope.newMonthly = row.monthlyExpense;
+          $scope.newYearly = row.yearlyExpense;
 
-  $("#deleteComments").click(function(){
-    $("#json").text("Delete");
-    var url = "comment";
-    $.ajax({
-      url:url,
-      type: "DELETE",
-      success: function(data,textStatus) {
-        $("#done").html(textStatus);
-        $("#comments").html('');
+          $scope.rows.push({
+            name: row.name,
+            monthlyExpense: row.monthlyExpense,
+            yearlyExpense: row.yearlyExpense,
+            percent: ((row.monthlyExpense / $scope.incomeInput.monthlyIncome) * 100)
+          });
+
+          row.name = '';
+          row.monthlyExpense = '';
+          row.yearlyExpense = '';
+
+           var totalMonthlyExpenses = 0;
+
+            for(var i=0; i<$scope.rows.length; i++){
+                totalMonthlyExpenses += $scope.rows[i].monthlyExpense;
+            }
+
+            var savingsTotal = ($scope.incomeInput.monthlyIncome - $scope.monthlyTithing);
+            savingsTotal -= totalMonthlyExpenses;
+
+            $scope.monthlySavings = savingsTotal;
+            $scope.yearlySavings = (savingsTotal*12);
+            $scope.savingsPercent = (savingsTotal/$scope.incomeInput.monthlyIncome * 100);
       }
-    })
-  });
 
-  $("#getComments").click(function() {
-    $.getJSON('comment', function(data) {
-      console.log(data);
-      var everything = "<ul>";
-      for(var comment in data) {
-        com = data[comment];
-        everything += "<li> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
-      }
-      everything += "</ul>";
-      $("#json").text("Get");
-      $("#done").html("success");
-      $("#comments").html(everything);
-    })
-  });
+    };
+  }
 
-});
+
+// $(document).ready(function(){
+//
+//   $("#postComment").click(function(){
+//     var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
+//     jobj = JSON.stringify(myobj);
+//     $("#json").text(jobj);
+//     var url = "comment";
+//     $.ajax({
+//       url:url,
+//       type: "POST",
+//       data: jobj,
+//       contentType: "application/json; charset=utf-8",
+//       success: function(data,textStatus) {
+//         $("#done").html(textStatus);
+//       }
+//     })
+//   });
+//
+//   $("#deleteComments").click(function(){
+//     $("#json").text("Delete");
+//     var url = "comment";
+//     $.ajax({
+//       url:url,
+//       type: "DELETE",
+//       success: function(data,textStatus) {
+//         $("#done").html(textStatus);
+//         $("#comments").html('');
+//       }
+//     })
+//   });
+//
+//   $("#getComments").click(function() {
+//     $.getJSON('comment', function(data) {
+//       console.log(data);
+//       var everything = "<ul>";
+//       for(var comment in data) {
+//         com = data[comment];
+//         everything += "<li> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+//       }
+//       everything += "</ul>";
+//       $("#json").text("Get");
+//       $("#done").html("success");
+//       $("#comments").html(everything);
+//     })
+//   });
+//
+// });
