94a4d16a15a5df9e276bb7698988b93174c2745a
peytontw <wilkiejr29@gmail.com>
2017-11-17 00:08:33 +0000

    second commit

---
+++ comments/routes/index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/commentDB',{useMongoClient:true});
+var commentSchema = mongoose.Schema({ //Defines the Schema for this database
+  Name: String,
+  Comment: String
+});
+
+var Comment = mongoose.model('Comment', commentSchema); //Makes an object from that schema as a model
+
+var db = mongoose.connection; //Saves the connection as a variable to use
+db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+db.once('open', function() { //Lets us know when we're connected
+console.log('Connected');
+});
+
+/* GET home page. */
+router.get('/fake', function(req, res, next) {
+  console.log("Inside Fake");
+  var fakelist=[{Name:"Jim",Comment:"Hi"}];
+  res.render('index', { title: 'Express' });
+});
+
+/* GET comments from database */
+router.get('/comment', function(req, res, next) {
+console.log("In the GET route? yes");
+Comment.find(function(err,commentList) { //Calls the find() method on your database
+  if (err) return console.error(err); //If there's an error, print it out
+  else {
+    console.log(commentList); //Otherwise console log the comments you found
+    res.json(commentList);
+  }
+})
+});
+
+router.delete('/comment', function(req,res,next) {
+  console.log("Delete");
+  Comment.remove(function(err) {
+    if(err) return console.error(err);
+    else {
+      console.log("Delete worked");
+      res.sendStatus(200);
+    }
+  })
+});
+
+router.post('/comment', function(req, res, next) {
+  console.log("POST comment route"); //[1]
+  console.log(req.body);
+  var newcomment = new Comment(req.body); //[3]
+  console.log(newcomment); //[3]
+  newcomment.save(function(err, post) { //[4]
+    if (err) return console.error(err);
+    console.log(post);
+    console.log(req.body); //[2]
+    res.sendStatus(200);
+  });
+});
+
+module.exports = router;
